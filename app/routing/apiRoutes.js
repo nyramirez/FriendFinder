75945ada1bd89029402b0a5ca4f057ed4e178370
Nefas <nyramirez@gmail.com>
2018-11-14 20:46:22 -0600

    correction of console.log errors

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,11 @@
 let friendList = require("../data/friends");
 
-module.exports = function(app) {
-    app.get("/api/friends", function (req, res){
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
         res.json(friendList);
     });
 
-    app.post("/api/friends", function(req,res){
+    app.post("/api/friends", function (req, res) {
         friendList.push(req.body);
         res.json(true);
     });
