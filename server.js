a85328252c7691d16f6d4e170023ff52a344d359
Nefas <nyramirez@gmail.com>
2018-12-19 20:07:50 -0600

    getting heroku ready

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let express = require("express");
 
 let app = express();
-let PORT = process.env.PORT || 8080;
+let PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
